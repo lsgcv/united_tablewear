e28703b9d850b2bdd1520e56221372fc71031c13
lsgcv <60223588+lsgcv@users.noreply.github.com>
2020-12-30 21:02:42 +0100

    added #

--- UA_Yolo4/network.c
+++ UA_Yolo4/network.c
@@ -374,7 +374,7 @@ float train_network_datum(network net, float *x, float *y)
     float error = get_network_cost(net);
     //if(((*net.seen)/net.batch)%net.subdivisions == 0) update_network(net);
     if(*(state.net.total_bbox) > 0)
-       // fprintf(stderr, " total_bbox = %d, rewritten_bbox = %f %% \n", *(state.net.total_bbox), 100 * (float)*(state.net.rewritten_bbox) / *(state.net.total_bbox));
+        fprintf(stderr, " #total_bbox = %d, rewritten_bbox = %f %% \n", *(state.net.total_bbox), 100 * (float)*(state.net.rewritten_bbox) / *(state.net.total_bbox));
     return error;
 }
 
